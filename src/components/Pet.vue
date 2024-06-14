a4865f4ef3e12cbb0fce3643610c75878a891053
CandaceAndrews <candacewolfandrews@hotmail.com>
2024-06-13 20:56:46 -0400

    pet.vue component

--- src/components/Pet.vue
+++ src/components/Pet.vue
@@ -1,1 +1,19 @@
 <!-- Displays the pet and its status (happiness, hunger, etc.) -->
+
+<template>
+    <div>
+        <h1>{{  pet.name }}</h1>
+        <p>Hunger: {{ pet.hunger }}</p>
+        <p>Happiness: {{ pet.happiness }}</p>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+    computed: {
+        ...mapGetters(['pet']),
+    },
+};
+</script>
