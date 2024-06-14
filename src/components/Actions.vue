89d0e619092a7d45c2e367cd57a6830c48cddd41
CandaceAndrews <candacewolfandrews@hotmail.com>
2024-06-13 20:59:35 -0400

    actions.vue component for interaction with pet

--- src/components/Actions.vue
+++ src/components/Actions.vue
@@ -1,1 +1,17 @@
 <!-- Buttons or controls for interacting with the pet (feeding, playing, etc.) -->
+ <template>
+    <div>
+        <button @click="feedPet">Feed</button>
+        <button @click="playWithPet">Play</button>
+    </div>
+ </template>
+
+ <script>
+import { mapActions } from 'vuex';
+
+export default {
+    methods: {
+        ...mapActions(['feedPet', 'playWithPet']),
+    },
+};
+</script>
