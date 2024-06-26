7ecc18fd49d7c90ba176ce6ce5bbbe6c36f1606e
CandaceAndrews <candacewolfandrews@hotmail.com>
2024-06-26 18:40:17 -0400

    adjusted pet stats for various screen sizes

--- src/components/PetStats.vue
+++ src/components/PetStats.vue
@@ -20,12 +20,14 @@ export default {
 
 <style scoped>
 .pet-stats {
-  background-color: #97d2b3; /* A light wheat color */
+  background-color: #97d2b3; 
   border-radius: 10px;
   padding: 20px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   width: 20%;
   height: auto;
+  max-width: 300px; /* Maximum width to maintain layout */
+  min-width: 200px; /* Minimum width for smaller screens */
   position: absolute;
   left: 20px; /* Position to the left of the screen */
   top: 20px; /* Add some margin from the top */
@@ -46,4 +48,24 @@ export default {
   background-color: #dbffdb; /* Slightly lighter background for the stats */
   border-radius: 5px;
 }
+
+/* Responsive text sizing for the pet's name */
+@media (max-width: 1024px) {
+  .name {
+    font-size: 1.4em;
+  }
+}
+
+@media (max-width: 767px) {
+  .name {
+    font-size: 1.2em;
+  }
+}
+
+@media (max-width: 480px) {
+  .name {
+    font-size: 1em;
+  }
+}
+
 </style>
