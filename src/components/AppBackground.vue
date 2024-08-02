a2debc326bd67ccf53b301e1a688578193094c15
CandaceAndrews <candacewolfandrews@hotmail.com>
2024-08-01 21:32:04 -0400

    dark overlay when pet dies

--- src/components/AppBackground.vue
+++ src/components/AppBackground.vue
@@ -1,12 +1,19 @@
  <template>
     <div class="background">
-        <slot></slot>
+      <div v-if="isDead" class="dim-overlay"></div>
+      <slot></slot>
     </div>
  </template>
 
  <script>
 export default {
     name: 'AppBackground',
+    props: {
+      isDead: {
+        type: Boolean,
+        default: false
+      }
+    }
 };
 </script>
 
@@ -19,5 +26,15 @@ export default {
   height: 600px;
   overflow: hidden;
   position: relative; /* Allows absolute positioning of child elements */
+}
+
+.dim-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.7);
+  z-index: 5;
 }
 </style>
